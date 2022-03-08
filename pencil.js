afce5b50cc74766fd645b350112a41a3b2b2ae0e
Cocoandkratos <81406537+Cocoandkratos@users.noreply.github.com>
2022-03-08 19:48:50 +0530

    Add files via upload

---
+++ pencil.js
@@ -0,0 +1,30 @@
+Status = "";
+pencil_image = "";
+
+function preload(){
+    pencil_image = loadImage("pencil.jpg");
+}
+
+function setup(){
+    canvas = createCanvas(640,350);
+    canvas.position(315,200);
+    object_Detector = ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+}
+
+function modelLoaded(){
+    console.log("Model Loaded!");
+    Status = true;
+    object_Detector.detect(pencil_image,gotResults);
+}
+
+function gotResults(error,results){
+    if(error){
+        console.error(error);
+    }
+    console.log(results);
+}
+
+function draw(){
+    image(pencil_image,0,0,640,350);
+}
